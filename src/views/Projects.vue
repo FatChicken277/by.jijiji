39d6052b734e737ee522a4e366a8dbe79a6fbeb0
danniebravo <contacto.interfarm@gmail.com>
2026-03-21 19:52:48 -0500

    fix: Projects.vue espera respuesta async de Firebase

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -10,6 +10,7 @@ const { getAll } = useProjects();
 
 const allCards = ref([]);
 const displayCards = ref([]);
+const loading = ref(true);
 let from = 0;
 const BATCH = 9;
 
@@ -41,10 +42,11 @@ function handleScroll() {
   }
 }
 
-onMounted(() => {
-  allCards.value = getAll();
+onMounted(async () => {
+  allCards.value = await getAll();
   from = 0;
   displayCards.value = getNextBatch();
+  loading.value = false;
   window.addEventListener("scroll", handleScroll);
   window.addEventListener("resize", () => { screenWidth.value = window.innerWidth; });
 });
@@ -52,6 +54,12 @@ onMounted(() => {
 
 <template>
   <div>
+    <!-- Loading -->
+    <div v-if="loading" class="absolute top-0 z-[15] flex h-[100dvh] w-[100dvw] items-center justify-center bg-black">
+      <h1>Loading...</h1>
+    </div>
+
+    <!-- Video popup -->
     <div
       v-if="popupIsActive"
       @wheel.prevent
@@ -78,75 +86,79 @@ onMounted(() => {
       </div>
     </div>
 
-    <div class="mx-5 mt-5 shadow-[0_0_40px_0_rgba(255,255,255,0.4)]">
-      <carousel
-        :autoplay="1000"
-        :transition="1000"
-        :items-to-show="itemsToShow"
-        :wrap-around="true"
-        :pauseAutoplayOnHover="true"
-      >
-        <slide v-for="(card, index) in allCards.filter((c) => c.highlight)" :key="index">
-          <div
-            @click="openVideo(card.yt)"
-            class="group relative my-2 flex aspect-video w-full cursor-pointer flex-col md:my-0 md:items-center md:justify-center"
-          >
-            <VueLoadImage class="w-full">
-              <template v-slot:image>
-                <img
-                  class="aspect-video w-full object-cover transition ease-in-out md:group-hover:opacity-10 md:group-hover:blur-sm"
-                  :alt="card.title"
-                  :src="`https://img.youtube.com/vi/${card.yt}/hqdefault.jpg`"
-                />
-              </template>
-              <template v-slot:preloader>
-                <div class="flex h-[10rem] w-full items-center justify-center">
-                  <img class="h-10 w-10" src="../assets/loading.gif" />
-                </div>
-              </template>
-            </VueLoadImage>
-            <div class="hidden flex-col break-words md:absolute md:items-center md:text-center md:group-hover:flex">
-              <h1 class="mt-2 box-content break-words text-xl font-bold md:mt-0">
-                {{ card.title.toUpperCase() }}
-              </h1>
-              <h2 class="text-xs">{{ card.work.toUpperCase() }}</h2>
+    <div v-if="!loading">
+      <!-- Highlights carousel -->
+      <div class="mx-5 mt-5 shadow-[0_0_40px_0_rgba(255,255,255,0.4)]">
+        <carousel
+          :autoplay="1000"
+          :transition="1000"
+          :items-to-show="itemsToShow"
+          :wrap-around="true"
+          :pauseAutoplayOnHover="true"
+        >
+          <slide v-for="(card, index) in allCards.filter((c) => c.highlight)" :key="index">
+            <div
+              @click="openVideo(card.yt)"
+              class="group relative my-2 flex aspect-video w-full cursor-pointer flex-col md:my-0 md:items-center md:justify-center"
+            >
+              <VueLoadImage class="w-full">
+                <template v-slot:image>
+                  <img
+                    class="aspect-video w-full object-cover transition ease-in-out md:group-hover:opacity-10 md:group-hover:blur-sm"
+                    :alt="card.title"
+                    :src="`https://img.youtube.com/vi/${card.yt}/hqdefault.jpg`"
+                  />
+                </template>
+                <template v-slot:preloader>
+                  <div class="flex h-[10rem] w-full items-center justify-center">
+                    <img class="h-10 w-10" src="../assets/loading.gif" />
+                  </div>
+                </template>
+              </VueLoadImage>
+              <div class="hidden flex-col break-words md:absolute md:items-center md:text-center md:group-hover:flex">
+                <h1 class="mt-2 box-content break-words text-xl font-bold md:mt-0">
+                  {{ card.title.toUpperCase() }}
+                </h1>
+                <h2 class="text-xs">{{ card.work.toUpperCase() }}</h2>
+              </div>
             </div>
-          </div>
-        </slide>
-        <template #addons>
-          <navigation />
-          <pagination />
-        </template>
-      </carousel>
-    </div>
+          </slide>
+          <template #addons>
+            <navigation />
+            <pagination />
+          </template>
+        </carousel>
+      </div>
 
-    <h1 class="my-5 text-center text-xl font-bold md:my-10">Latest releases</h1>
-    <div class="grid grid-cols-1 gap-3 px-5 md:grid-cols-2 xl:grid-cols-3">
-      <div
-        v-for="card in displayCards"
-        :key="card.id ?? card.title"
-        @click="openVideo(card.yt)"
-        class="group relative my-2 flex aspect-video w-full cursor-pointer flex-col md:my-0 md:items-center md:justify-center"
-      >
-        <VueLoadImage class="w-full">
-          <template v-slot:image>
-            <img
-              class="aspect-video w-full object-cover transition ease-in-out md:group-hover:opacity-10 md:group-hover:blur-sm"
-              :alt="card.title"
-              :src="`https://img.youtube.com/vi/${card.yt}/hqdefault.jpg`"
-            />
-          </template>
-          <template v-slot:preloader>
-            <div class="flex h-[10rem] w-full items-center justify-center">
-              <img class="h-10 w-10" src="../assets/loading.gif" />
-            </div>
-          </template>
-        </VueLoadImage>
-        <div class="flex flex-col break-words group-hover:flex md:absolute md:hidden md:items-center md:text-center">
-          <h1 class="mt-2 box-content break-words text-xl font-bold md:mt-0">
-            {{ card.title.toUpperCase() }}
-          </h1>
-          <h2 class="text-xs">{{ card.work.toUpperCase() }}</h2>
+      <!-- Grid -->
+      <h1 class="my-5 text-center text-xl font-bold md:my-10">Latest releases</h1>
+      <div class="grid grid-cols-1 gap-3 px-5 md:grid-cols-2 xl:grid-cols-3">
+        <div
+          v-for="card in displayCards"
+          :key="card.id ?? card.title"
+          @click="openVideo(card.yt)"
+          class="group relative my-2 flex aspect-video w-full cursor-pointer flex-col md:my-0 md:items-center md:justify-center"
+        >
+          <VueLoadImage class="w-full">
+            <template v-slot:image>
+              <img
+                class="aspect-video w-full object-cover transition ease-in-out md:group-hover:opacity-10 md:group-hover:blur-sm"
+                :alt="card.title"
+                :src="`https://img.youtube.com/vi/${card.yt}/hqdefault.jpg`"
+              />
+            </template>
+            <template v-slot:preloader>
+              <div class="flex h-[10rem] w-full items-center justify-center">
+                <img class="h-10 w-10" src="../assets/loading.gif" />
+              </div>
+            </template>
+          </VueLoadImage>
+          <div class="flex flex-col break-words group-hover:flex md:absolute md:hidden md:items-center md:text-center">
+            <h1 class="mt-2 box-content break-words text-xl font-bold md:mt-0">
+              {{ card.title.toUpperCase() }}
+            </h1>
+            <h2 class="text-xs">{{ card.work.toUpperCase() }}</h2>
+          </div>
         </div>
       </div>
     </div>
